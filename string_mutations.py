3abec70979afcf6a3216fcdf5976420cc5bbb4b9
priyadharshini13 <dharshini.dp95@gmail.com>
2021-06-17 08:28:31 +0100

    Create string_mutations.py
    Task
        Read a given string, change the character at a given index and then print the modified string.
    
    Function Description
    Complete the mutate_string function in the editor below.
    mutate_string has the following parameters:
        string string: the string to change
        int position: the index to insert the character at
        string character: the character to insert
    Returns
       string: the altered string

---
+++ string_mutations.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/python-mutations/problem
+# Sample Input
+# STDIN           Function
+# -----           --------
+# abracadabra     s = 'abracadabra'
+# 5 k             position = 5, character = 'k'
+
+# Sample Output
+#   abrackdabra
+def mutate_string(string, position, character):
+    
+    # Solution 1
+    # mutated_string = string[:position] + character + string[position+1:]
+    
+    # Solution 2
+    # convert string to list as string is immutable, replace the character and then join the list elements
+    # list_string = list(string)
+    # list_string[position] = character
+    # mutated_string = ''.join(list_string)
+    # return mutated_string
+
+    # Solution 3
+    return ("{}{}{}".format(string[:position], character, string[position+1:]))
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
