68268ae47a3baa35632b8aab0801d200ad787667
priyadharshini13 <dharshini.dp95@gmail.com>
2021-11-08 23:59:15 +0000

    Create Intersection_Set.py

---
+++ Intersection_Set.py
@@ -0,0 +1,4 @@
+.intersection()
+The .intersection() operator returns the intersection of a set and the set of elements in an iterable.
+Sometimes, the & operator is used in place of the .intersection() operator, but it only operates on the set of elements in set.
+The set is immutable to the .intersection() operation (or & operation).
